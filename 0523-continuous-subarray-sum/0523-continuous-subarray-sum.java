87ab61886c5a2aa48076667449e81a183bf52502
Pradip Nemane <pradipnemane@gmail.com>
2023-10-27 19:36:41 -0400

    Time: 22 ms (47.08%), Space: 57.4 MB (72.33%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer,Integer> prefixMap = new HashMap<>();
+        prefixMap.put(0,0);
+        int sum = 0;
+        for(int i=0;i< nums.length;i++){
+            sum+=nums[i];
+            int key = sum%k ;
+            if(!prefixMap.containsKey(key)){
+                prefixMap.put(key,i+1);
+            } else if(prefixMap.get(key)<i){
+                return true;
+            }
+        }
+        return false;
+    }
+}
