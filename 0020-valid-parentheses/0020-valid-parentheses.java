fc7e505609685cf7ba5ae3cd161b7e3faff51095
Pradip Nemane <pradipnemane@gmail.com>
2023-10-28 18:32:19 -0400

    Time: 2 ms (83.23%), Space: 40.7 MB (17.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(char ch : s.toCharArray()){
-
-            if(ch=='('|| ch =='{' || ch=='['){
+        Stack<Character> stack= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
                 stack.push(ch);
-            } else {
-                if(!stack.isEmpty()){
-                    char top = stack.peek();
-                    if((top == '(' && ch==')' ) || (top =='[' && ch==']') || (top =='{' && ch=='}')){
-                        stack.pop();
-                    } else {
-                        stack.push(ch);
-                    }
-                }else {
-                     stack.push(ch);
-                }
-                
+                continue;
+            }else if(stack.isEmpty()){
+                return false;
+            }
+            char c = stack.pop();
+            if(ch!=')'&& c=='(' || ch!='}'&& c=='{' || ch!=']'&& c==']'){
+                return false;
             }
         }
-        return stack.size()==0;
+        return stack.isEmpty();
     }
 }
