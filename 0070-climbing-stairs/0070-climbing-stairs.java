f86048d58165312634dbfd8e5a7f751e68543368
Pradip Nemane <pradipnemane@gmail.com>
2023-10-29 10:31:56 -0400

    Time: 0 ms (100%), Space: 39.5 MB (14.31%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    int climbStairs(int n){
+    int[] memo = new int [n+1];
+        return climbStairs( n, memo);
+    }
+    public int climbStairs(int n, int[] memo) {
+        if(memo[n]!=0){
+            return memo[n];
+        }
+        if(n==0) {
+             memo[n]= 0;
+            return memo[n];
+        }
+        if(n==1) {
+          memo[1] = 1;
+          return memo[n];
+        }if(n==2) {
+             memo[n]= 2;
+            return  memo[n];
+        }
+        memo[n]= climbStairs(n-1,memo) + climbStairs(n-2,memo);
+        return memo[n];
+    }
+}
