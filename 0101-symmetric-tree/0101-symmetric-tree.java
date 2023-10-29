9811d9da62955e280f9ba74e8749693df252c353
Pradip Nemane <pradipnemane@gmail.com>
2023-10-28 22:47:44 -0400

    Time: 0 ms (100%), Space: 40.9 MB (22.27%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -18,12 +18,12 @@ class Solution {
        return isSymmetric(root.left,root.right);
     }
     public boolean isSymmetric(TreeNode root1 , TreeNode root2){
-        if(root1==null && root2==null){
+        if(root1 == null && root2 == null){
             return true;
         }
-        if(root1== null || root2==null){
+        if(root1 == null || root2 ==null) {
             return false;
         }
-        return root1.val== root2.val && isSymmetric(root1.left,root2.right)&& isSymmetric(root1.right,root2.left);
+        return root1.val == root2.val && isSymmetric(root1.left,root2.right) && isSymmetric(root1.right,root2.left);
     }
 }
