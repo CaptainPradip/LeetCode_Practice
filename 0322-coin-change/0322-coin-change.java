3b6efba9b27a65b60516aa577e237d4af86c98ba
Pradip Nemane <pradipnemane@gmail.com>
2023-11-03 23:08:49 -0400

    Time: 14 ms (86.44%), Space: 43.2 MB (52.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,29 +1,15 @@
 class Solution {
-    private Integer[] memo;
-
     public int coinChange(int[] coins, int amount) {
-
-       memo = new Integer [amount+1];
-       return coinChangeHelper(coins,amount);
-
-    }
-    public int coinChangeHelper(int[] coins, int amount) {
-        if(amount<0){
-            return -1;
-        }
-        if(amount==0){
-            return 0;
-        }
-        if(memo[amount]!=null) return memo[amount];
-        int minCount = Integer.MAX_VALUE;
-        for(int coin: coins) {
-            int count = coinChangeHelper(coins, amount-coin);
-            if(count!=-1){
-                minCount=Math.min(minCount,count+1);
+        int [] dp = new int [amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for(int i=1; i<= amount;i++){
+            for(int coin: coins){
+                if(i-coin >= 0){
+                    dp[i]= Math.min(dp[i],dp[i-coin] + 1);
+                }
             }
         }
-        memo[amount] =minCount==Integer.MAX_VALUE ?-1:minCount;
-        return memo[amount];
+        return dp[amount]== (amount+1)?-1:dp[amount];
     }
-
 }
