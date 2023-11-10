e1a7a74a093a298de64fca712814c2fa63715e88
Pradip Nemane <pradipnemane@gmail.com>
2023-11-09 19:40:31 -0500

    Time: 2 ms (92.39%), Space: 55.4 MB (37.68%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        // if you have gas with buying all gas with the cost that means you can travel to circular thats it. 
+	 // we need to find the starting point if you dont have the gas then your that point will be next point
+
+	 int startPoint = 0;
+	 int currentGas = 0;
+	 int totalGas = 0;
+	 for(int i =0 ;i< cost.length;i++){
+		 currentGas+=gas[i]-cost[i];
+		 totalGas += gas[i]-cost[i];
+		 if(currentGas<0){
+			 currentGas=0;
+			 startPoint = i+1;
+		 }
+	 }
+	 return totalGas < 0 ?-1 : startPoint;
+		
+    }
+}
