e15c24fff12cfd2989077b0c68152d48c6ebd014
Pradip Nemane <pradipnemane@gmail.com>
2023-11-10 18:33:10 -0500

    Time: 37 ms (94.35%), Space: 57.8 MB (31.86%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -6,10 +6,14 @@ class Solution {
         int index = 0;
         int m = potions.length;
         int maxPotion = potions[m - 1];
-        for(int spell : spells ){
+        for(int spell : spells){
+
+            // Minimum value of potion whose product with current spell  
+            // will be at least success or more.
             long minPotion = (long) Math.ceil((1.0 * success) / spell);
             int left = 0;
             int right = potions.length;
+            // what if the min potion is max than the max potions 
             if (minPotion > maxPotion) {
                 result[index++] = 0;
                 continue;
