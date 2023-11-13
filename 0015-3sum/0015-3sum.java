f9ce8ffe8b83295cb9320b3964778bb1a8c3beb4
Pradip Nemane <pradipnemane@gmail.com>
2023-11-12 21:55:20 -0500

    Time: 28 ms (83.68%), Space: 52.1 MB (9.87%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // [-1,0,1,2,-1,-4]
-        // -4,-1,-1,0,1,2
+        List<List<Integer>> result = new LinkedList<>();
         Arrays.sort(nums);
-         List<List<Integer>> result = new ArrayList<>();
-        // sort the array
-        // remove duplicate 
-        //  got with left and right  pointer and check the sum =0 
-        // add the number 
-        int n=nums.length;
-        for(int i=0; i< n ;i++){
-            while(i!=0 &&  i < n && nums[i]==nums[i-1] ){
-                i++;
+        for(int i=0;i<nums.length;i++){
+            
+            if(i!=0 && nums[i]==nums[i-1])
+            {
+                continue;
             }
             int left = i+1;
-            int right = n-1;
-            
-            while(left < right){
-               int  sum = nums[i]+ nums[left] + nums[right];
-               if(sum==0){
-                   List<Integer> subResult = new ArrayList(Arrays.asList(nums[i], nums[left++],nums[right--]));
-                   while(left< right && nums[left]==nums[left-1]){
-                       left++;
-                   }
-                   result.add(subResult);
-               }else if(sum <0){
-                   left++;
-               }else {
+            int right = nums.length-1;
+            while(left<right){
+                int val = nums[i] + nums[left] + nums[right];
+                if(val==0){
+                     List<Integer> temp = new LinkedList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[left++]);
+                    temp.add(nums[right--]);
+                    result.add(temp);
+                    while (left < right && nums[left] == nums[left - 1])
+                        ++left;
+                    //break;
+                } else if(val < 0){
+                    left++;
+                } else{
                    right--;
-               }
+                }
             }
+          
         }
         return result;
     }
