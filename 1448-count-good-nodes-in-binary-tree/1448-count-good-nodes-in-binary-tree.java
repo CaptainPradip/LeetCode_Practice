a7e52fb1f0eae70ba859add100f537cad8bbeadf
Pradip Nemane <pradipnemane@gmail.com>
2024-01-12 16:36:39 -0500

    Time: 2 ms (68.75%), Space: 54.3 MB (5.5%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -1,31 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public int goodNodes(TreeNode root) {
-        
-        return  getGoodNodes(root,root.val);
+
+        return getGoodNodes(root, root.val);
     }
-    public int getGoodNodes(TreeNode root, int maxValue){
-        if(root==null){
+
+    public int getGoodNodes(TreeNode root, int maxValue) {
+        if (root == null) {
             return 0;
         }
-        if(root.val>=maxValue ){
-            return 1 +  getGoodNodes(root.left,Math.max(root.val,maxValue)) + getGoodNodes(root.right,Math.max(root.val,maxValue));
+        if (root.val >= maxValue) {
+            return 1 + getGoodNodes(root.left, Math.max(root.val, maxValue))
+                    + getGoodNodes(root.right, Math.max(root.val, maxValue));
         }
-          return  getGoodNodes(root.left,Math.max(root.val,maxValue)) + getGoodNodes(root.right,Math.max(root.val,maxValue));
+        return getGoodNodes(root.left, Math.max(root.val, maxValue))
+                + getGoodNodes(root.right, Math.max(root.val, maxValue));
 
     }
 }
