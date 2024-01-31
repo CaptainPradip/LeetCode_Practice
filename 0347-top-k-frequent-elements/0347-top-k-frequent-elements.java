662a306766a2dd9c69536e14ac45d1ee72691e0c
Pradip Nemane <pradipnemane@gmail.com>
2024-01-31 17:25:56 -0500

    Time: 16 ms (35.39%), Space: 49.1 MB (9.11%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-    HashMap<Integer, Integer> hashMap = new HashMap<>();
-		for (int i = 0; i < nums.length; i++) {
-			hashMap.put(nums[i], hashMap.getOrDefault(nums[i], 0) + 1);
-		}
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(
-				(a, b) -> hashMap.get(a) - hashMap.get(b));
+        for (int i = 0; i < nums.length; i++) {
+            hashMap.put(nums[i], hashMap.getOrDefault(nums[i], 0) + 1);
+        }
 
-		int[] result = new int[k];
-		for (int key : hashMap.keySet()) {
-			minHeap.add(key);
-			if (minHeap.size() > k) {
-				minHeap.remove();
-			}
-		}
-		for(int i=0; i< k ;i++) {
-			result[i]= minHeap.remove();
-		}
-		return result;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(
+                (a, b) -> hashMap.get(a) - hashMap.get(b));
+
+        int[] result = new int[k];
+        for (int key : hashMap.keySet()) {
+            minHeap.add(key);
+            if (minHeap.size() > k) {
+                minHeap.remove();
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            result[i] = minHeap.remove();
+        }
+        return result;
     }
 }
