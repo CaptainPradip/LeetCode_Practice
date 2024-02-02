5e23a0f22f63ced20d472ba28c8f2464e162defc
Pradip Nemane <pradipnemane@gmail.com>
2024-02-02 13:10:15 -0500

    Time: 0 ms (100%), Space: 41.4 MB (36.23%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        String str = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != goal.charAt(0)) {
-                str = str + s.charAt(i);
-            } else {
-                String temp = s.substring(i , s.length()) + str;
-                if (temp.equals(goal)) {
-                    return true;
-                }
-                str = str + s.charAt(i);
-            }
-        }
-
-        return false;
+       return s.length()==goal.length() && (s+s).contains(goal);
     }
 }
