33794eccbc303919fa9b76c922fa52adba7e3b91
Pradip Nemane <pradipnemane@gmail.com>
2024-02-04 18:41:36 -0500

    Time: 13 ms (36.89%), Space: 45.4 MB (23.51%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,39 +1,39 @@
 class Solution {
-   public int calculate(String s) {
-		Stack<Integer> stack = new Stack<Integer>();
-		int result = 0;
-		int sign = 1;
-		int operand = 0;
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			if (Character.isDigit(ch)) {
-			    operand = 10 * operand + Integer.valueOf(ch+"");
-			} else if (ch == '+') {
-				result += sign * operand;
-				sign = 1;
-				operand = 0;
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int result = 0;
+        int sign = 1;
+        int operand = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                operand = 10 * operand + Integer.valueOf(ch + "");
+            } else if (ch == '+') {
+                result += sign * operand;
+                sign = 1;
+                operand = 0;
 
-			} else if (ch == '-') {
-				result += sign * operand;
-				sign = -1;
-				operand = 0;
+            } else if (ch == '-') {
+                result += sign * operand;
+                sign = -1;
+                operand = 0;
 
-			} else if (ch == '(') {
-				stack.push(result); 
-				stack.push(sign);
-				sign = 1;
-				result = 0;
+            } else if (ch == '(') {
+                stack.push(result);
+                stack.push(sign);
+                sign = 1;
+                result = 0;
 
-			} else if (ch == ')') {
-				result += sign * operand;
-				result *= stack.pop();
-				result += stack.pop();
-				operand = 0;
-			}
+            } else if (ch == ')') {
+                result += sign * operand;
+                result *= stack.pop();
+                result += stack.pop();
+                operand = 0;
+            }
 
-		}
-		return result + (sign * operand);
+        }
+        return result + (sign * operand);
 
-	}
+    }
 
 }
