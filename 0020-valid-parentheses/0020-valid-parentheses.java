6bbe86047842e99c0672f194162e7aed9de2cf54
Pradip Nemane <pradipnemane@gmail.com>
2024-02-09 14:39:50 -0500

    Time: 1 ms (98.77%), Space: 41.4 MB (46.06%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+        if (s.length() % 2 != 0)
+            return false;
         for (char ch : s.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
