5aad2e02c6cec65972ccbeb1cb14bebf912f822a
Pradip Nemane <pradipnemane@gmail.com>
2024-02-13 15:30:32 -0500

    Time: 1 ms (11.79%), Space: 42.6 MB (49.6%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -1,48 +1,42 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
- class Pair{
-    public TreeNode node;
-    public int val;
-     Pair(TreeNode node,int val){
-         this.node = node;
-         this.val = val;
-     }
- }
 class Solution {
     public int maxDepth(TreeNode root) {
-        Stack<Pair> stack= new Stack<>();
-        if(root==null){
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        int level = 0;
+        if (root == null) {
             return 0;
         }
-        stack.push(new Pair(root,1));
-        int level = 0;
-        while(!stack.isEmpty()){
-          //  for(int i=0 ;i<size;i++){
-                Pair currentNode = stack.pop();
-                if(currentNode.node.left==null && currentNode.node.right==null){
-                    level = Math.max(level,currentNode.val);
-                }
-                if(currentNode.node.left!=null){
-                    stack.push(new Pair(currentNode.node.left,currentNode.val+1));
-                }
-                if(currentNode.node.right!=null){
-                    stack.push(new Pair(currentNode.node.right,currentNode.val+1));
-                }
+        queue.add(root);
 
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.remove();
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+            level++;
         }
+
         return level;
+
     }
 }
