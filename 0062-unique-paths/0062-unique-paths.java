e1718f1050f92d78cc6fc554b5a66f740a917c42
Pradip Nemane <pradipnemane@gmail.com>
2024-02-15 13:00:05 -0500

    Time: 0 ms (100%), Space: 40.3 MB (50.7%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,16 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[] dp= new int [n];
+        int[][] dp= new int [m][n];
         for(int i=0; i<m; i++){
              for(int j=0; j<n; j++){
-                 if (j==0){
-                     dp[j]=1;
+                 if (i==0 || j== 0){
+                     dp[i][j]=1;
                  } else {
-                     dp[j]= dp[j] + dp[j-1];
+                     dp[i][j] = dp[i-1][j] + dp[i][j-1];
                  }
 
              }
         }
-        return dp[n-1];
+        return dp[m-1][n-1];
     }
 }
