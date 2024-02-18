f48c9ad835cd0129b2e03430fe3da61e0e89be48
Pradip Nemane <pradipnemane@gmail.com>
2024-02-18 14:30:48 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,39 +1,39 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        Deque<TreeNode> queue = new ArrayDeque<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
         List<Integer> result = new LinkedList<>();
-        if(root==null){
+        if (root == null) {
             return result;
         }
         queue.add(root);
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode currentNode = queue.pop();
-                if(currentNode.left!=null){
-                    queue.add(currentNode.left);
-                }
-                if(currentNode.right!=null){
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.remove();
+                if (currentNode.right != null) {
                     queue.add(currentNode.right);
                 }
-                if(i==size-1){
+                // if (currentNode.left != null) {
+                //     queue.add(currentNode.left);
+                // }
+               // if (i == 0) {
                     result.add(currentNode.val);
-                }
+               // }
             }
         }
         return result;
