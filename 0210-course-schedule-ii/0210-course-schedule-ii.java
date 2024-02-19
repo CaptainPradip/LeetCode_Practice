55720ce61e36a74385725f6a1da84ba8d83b4b39
Pradip Nemane <pradipnemane@gmail.com>
2024-02-18 23:35:04 -0500

    Time: 7 ms (48.32%), Space: 46.1 MB (28.05%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,43 +1,42 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-		List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0;i<numCourses;i++) {
-			graph.add(new ArrayList<>());
-		}
-		int[] indegree = new int [numCourses];
-		for(int[] pre: prerequisites) {
-			int start = pre[1];
-			int end = pre[0];
-			graph.get(start).add(end);
-			indegree[end]++;
-		}
-		Queue<Integer> queue = new LinkedList<>();
-		// add the all 0 indegaree courses 
-		 for(int i=0;i<numCourses;i++) {
-			 if(indegree[i]==0){
-				 queue.add(i);
-			 }
-		 }
-		 ArrayList<Integer> ordering = new ArrayList<>();
-		 int visited = 0 ;
-		 while(!queue.isEmpty()){
-			 int currentNode = queue.remove();
-			 visited++;
-			 ordering.add(currentNode);
-			 for(int node : graph.get(currentNode)){
-				 indegree[node]--;
-				 if(indegree[node]==0){
-					 queue.add(node);
-				 }
-			 }
-		 }
-		if(visited==numCourses){
-			return ordering.stream()
-    .mapToInt(Integer::intValue) // Convert Integer objects to int
-    .toArray();
-		}else{
-			return new int[0];
-		}
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        int[] indegree = new int[numCourses];
+        for (int[] pre : prerequisites) {
+            int start = pre[1];
+            int end = pre[0];
+            graph.get(start).add(end);
+            indegree[end]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        // add the all 0 indegaree courses
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        List<Integer> ordering = new ArrayList<>();
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            int currentNode = queue.remove();
+            visited++;
+            ordering.add(currentNode);
+            for (int node : graph.get(currentNode)) {
+                indegree[node]--;
+                if (indegree[node] == 0) {
+                    queue.add(node);
+                }
+            }
+        }
+        // check if correct we got the correct ordering
+        if (visited == numCourses) {
+            return ordering.stream().mapToInt(Integer::intValue).toArray();
+        } else {
+            return new int[0];
+        }
 
     }
 }
