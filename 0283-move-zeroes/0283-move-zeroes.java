66b7b47b11d89f2c6ff899c0a68df86af8d11b73
Pradip Nemane <pradipnemane@gmail.com>
2024-03-05 14:04:00 -0500

    Time: 2 ms (84.35%), Space: 46.1 MB (56.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       
+        int i = 0;
         int j = 0;
-        for(int i =1; i< nums.length;i++){
-            if(nums[j] == 0 && nums[i]!=0){
-                nums[j] = nums[i];
-                nums[i] = 0;
-                 j++;
-            } 
-            if(nums[j]!=0){
-                j++;
+        int n = nums.length;
+        while (j < n) {
+            if (nums[j] != 0 ) {
+                int temp = nums[i] ;
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
             }
+        
+            j++;
         }
     }
 }
