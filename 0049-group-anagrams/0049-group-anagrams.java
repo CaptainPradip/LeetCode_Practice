1670b50ea77ead1e4383210abfe6cbbe8e79586c
Pradip Nemane <pradipnemane@gmail.com>
2024-04-16 18:46:50 -0400

    Time: 7 ms (64.71%), Space: 47.5 MB (74.4%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] keys = str.toCharArray();
+            Arrays.sort(keys);
+            String key = String.valueOf(keys);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList(map.values());
+        
+    }
+}
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// "ate"    => soted : aet
+// "eat"    => soted : ate
+// "tea"    => soted : ate
