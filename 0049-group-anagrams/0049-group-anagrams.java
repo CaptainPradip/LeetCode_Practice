6fe0a131c5917269ca442e42721b0585caf2465c
Pradip Nemane <pradipnemane@gmail.com>
2024-04-16 19:02:06 -0400

    Time: 6 ms (97.2%), Space: 47.7 MB (62.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map = new HashMap<>();
-        for(String str : strs){
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
             char[] keys = str.toCharArray();
             Arrays.sort(keys);
             String key = String.valueOf(keys);
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
             }
             map.get(key).add(str);
         }
         return new ArrayList(map.values());
-        
     }
 }
 
 // Input: strs = ["eat","tea","tan","ate","nat","bat"]
 // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-// "ate"    => soted : aet
-// "eat"    => soted : ate
-// "tea"    => soted : ate
+
+// "eat" => Sort "aet" ;
+// "ate" => sort "aet" ;
