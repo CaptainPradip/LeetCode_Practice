56f27d457f8a8fc32fbd518ff1916261cab1fba4
Pradip Nemane <pradipnemane@gmail.com>
2024-04-18 20:55:33 -0400

    Time: 31 ms (22.2%), Space: 44.5 MB (22.38%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,26 +1,35 @@
+
 class Solution {
     public int characterReplacement(String s, int k) {
-        Map<Character, Integer> map = new HashMap<>(); 
+
         int left = 0;
         int right = 0;
         int n = s.length();
-        int max= Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
+        Map<Character, Integer> map = new HashMap<>();
         int maxFrequency = 0;
-        while(right < n){
+        while (right < n) {
             char ch = s.charAt(right);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-            maxFrequency = Math.max(maxFrequency,map.get(ch));
-            boolean isValid = right - left + 1 - maxFrequency > k ;
-            if(isValid) {
+            maxFrequency = Math.max(maxFrequency, map.get(ch));
+            boolean isValid = right - left + 1 - maxFrequency > k;
+            // condtion for left pointer or sliding window
+            if (isValid) {
                 char leftChar = s.charAt(left);
-                map.put(leftChar, map.get(leftChar)-1);
+                map.put(leftChar, map.get(leftChar) - 1);
                 left++;
             }
-            max= Math.max(max,right-left+1);
+            // result condition
+            max = Math.max(max, right - left + 1);
             right++;
         }
-
         return max;
 
     }
 }
+
+// s = "ABAB", k = 2
+// s = BBBB
+// s = "AABABBA", k = 1
+// left = 1
+// right= 1
