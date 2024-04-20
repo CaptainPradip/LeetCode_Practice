32995c169bae79fd7c7c84984152d4997e95f0a7
Pradip Nemane <pradipnemane@gmail.com>
2024-04-20 13:35:05 -0400

    Time: 13 ms (43.29%), Space: 44.7 MB (34.22%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        s = s.replaceAll("[^a-zA-Z0-9]","");
+        int j = s.length()-1;
+        while(i<=j){
+            if(Character.toUpperCase(s.charAt(j))!=Character.toUpperCase(s.charAt(i))){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+}
+
+// s = "A man, a plan, a canal: Panama"
+// "amanaplanacanalpanama"
+// 
