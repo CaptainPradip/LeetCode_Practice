cbca3f71dd7bf678906b7c800f11a6f994a4d721
Pradip Nemane <pradipnemane@gmail.com>
2024-04-22 23:30:45 -0400

    Time: 27 ms (91.18%), Space: 52.5 MB (23.46%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new LinkedList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            
-            if(i!=0 && nums[i]==nums[i-1])
-            {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int left = i+1;
-            int right = nums.length-1;
-            while(left<right){
-                int val = nums[i] + nums[left] + nums[right];
-                if(val==0){
-                     List<Integer> temp = new LinkedList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[left++]);
-                    temp.add(nums[right--]);
-                    result.add(temp);
-                    while (left < right && nums[left] == nums[left - 1])
-                        ++left;
-                    //break;
-                } else if(val < 0){
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    List<Integer> re = new ArrayList<>();
+                    re.add(nums[i]);
+                    re.add(nums[left++]);
+                    re.add(nums[right--]);
+                    result.add(re);
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                } else if (sum < 0) {
                     left++;
-                } else{
-                   right--;
+                } else {
+                    right--;
                 }
             }
-          
         }
         return result;
     }
 }
+
+// nums = [-1,0,1,2,-1,-4]
+// nums = [-4,-1,-1,0,1,2]
