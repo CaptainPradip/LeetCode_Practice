9b5c9e5824efb521616e28d3bd99d7b3452e6c52
Pradip Nemane <pradipnemane@gmail.com>
2024-06-29 22:59:00 -0400

    Time: 2 ms (66.66%), Space: 45.2 MB (37.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
-        int maxJump =0;
-		int n= nums.length;
-		int jumpCount=0;
-		int currentRange=0;
-		for(int i=0; i< n-1;i++){
-			maxJump = Math.max(maxJump,i+nums[i]);
-			if(currentRange == i){
-				 jumpCount++;
-				 currentRange = maxJump ;
-			}
-		}
-		return jumpCount;
+        int n = nums.length;
+        int maxJump = 0;
+        int currentJump = 0;
+        int jumps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            maxJump = Math.max(maxJump, i + nums[i]);
+            if (currentJump == i) {
+                jumps++;
+                currentJump = maxJump;
+            }
+        }
+        return jumps;
     }
 }
