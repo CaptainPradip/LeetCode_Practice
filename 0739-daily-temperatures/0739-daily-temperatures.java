837aaf10fbb710331ad819b125182ea97125d3a3
Pradip Nemane <pradipnemane@gmail.com>
2024-08-17 19:01:25 -0400

    Time: 61 ms (78.01%), Space: 63.1 MB (18.83%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        // 1. create a stack with the index
+        // 2. remove pop when temp is greater than the current
+        // 3. add the result value with the poped index with previousDay;
+        Stack<Integer> stack = new Stack<>();
+        int n = temperatures.length;
+        int[] result= new int [n];
+        for(int i =0;i< n ;i++){
+            int currentTemp = temperatures[i];
+
+            while(!stack.isEmpty()&& currentTemp > temperatures[stack.peek()]){
+                int prevDay = stack.pop();
+                result[prevDay]= i - prevDay;
+            }
+            stack.push(i);
+        }
+        return result;
+
+    }
+}
