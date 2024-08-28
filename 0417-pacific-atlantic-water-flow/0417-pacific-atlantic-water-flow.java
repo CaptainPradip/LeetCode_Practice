00f4cc681076de62142db31942e96ee5c12b42cc
Pradip Nemane <pradipnemane@gmail.com>
2024-08-28 00:05:04 -0400

    Time: 13 ms (28.13%), Space: 45.7 MB (38.25%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,74 +1,58 @@
 class Solution {
-    private static final int[][] DIRECTIONS = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    private int numRows;
-    private int numCols;
-    private int[][] landHeights;
-    
-    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
-        // Check if input is empty
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return new ArrayList<>();
-        }
+    int[][] DIRECTIONS = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    int rowNumber;
+    int colNumber;
+    int[][] matrix;
 
-        // Save initial values to parameters
-        numRows = matrix.length;
-        numCols = matrix[0].length;
-        landHeights = matrix;
-        
-        // Setup each queue with cells adjacent to their respective ocean
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+         rowNumber = heights.length;
+         colNumber = heights[0].length;
+         matrix = heights;
         Queue<int[]> pacificQueue = new LinkedList<>();
         Queue<int[]> atlanticQueue = new LinkedList<>();
-        for (int i = 0; i < numRows; i++) {
-            pacificQueue.offer(new int[]{i, 0});
-            atlanticQueue.offer(new int[]{i, numCols - 1});
+        for (int i = 0; i < rowNumber; i++) {
+            pacificQueue.offer(new int[] { i, 0 });
+            atlanticQueue.offer(new int[] { i, colNumber - 1 });
         }
-        for (int i = 0; i < numCols; i++) {
-            pacificQueue.offer(new int[]{0, i});
-            atlanticQueue.offer(new int[]{numRows - 1, i});
+        for (int i = 0; i < colNumber; i++) {
+            pacificQueue.offer(new int[] { 0, i });
+            atlanticQueue.offer(new int[] { rowNumber - 1, i });
         }
-        
-        // Perform a BFS for each ocean to find all cells accessible by each ocean
-        boolean[][] pacificReachable = bfs(pacificQueue);
-        boolean[][] atlanticReachable = bfs(atlanticQueue);
-        
-        // Find all cells that can reach both oceans
-        List<List<Integer>> commonCells = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                if (pacificReachable[i][j] && atlanticReachable[i][j]) {
-                    commonCells.add(List.of(i, j));
+        boolean[][] pacificVisited = BFS(pacificQueue, new boolean[rowNumber][colNumber]);
+        boolean[][] atlanticVisited = BFS(atlanticQueue, new boolean[rowNumber][colNumber]);
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < rowNumber; i++) {
+            for (int j = 0; j < colNumber; j++) {
+                if(pacificVisited[i][j] && atlanticVisited[i][j]){
+                    result.add(List.of(i,j));
                 }
             }
         }
-        return commonCells;
+        return result;
+
     }
-    
-    private boolean[][] bfs(Queue<int[]> queue) {
-        boolean[][] reachable = new boolean[numRows][numCols];
+
+    private boolean[][] BFS(Queue<int[]> queue, boolean[][] visited) {
         while (!queue.isEmpty()) {
             int[] cell = queue.poll();
-            // This cell is reachable, so mark it
-            reachable[cell[0]][cell[1]] = true;
-            for (int[] dir : DIRECTIONS) { // Check all 4 directions
-                int newRow = cell[0] + dir[0];
-                int newCol = cell[1] + dir[1];
-                // Check if new cell is within bounds
-                if (newRow < 0 || newRow >= numRows || newCol < 0 || newCol >= numCols) {
+            visited[cell[0]][cell[1]] = true;
+            for (int[] dir : DIRECTIONS) {
+                int newRow = dir[0] + cell[0];
+                int newCol = dir[1] + cell[1];
+                if (newRow < 0 || newRow >= rowNumber || newCol < 0 || newCol >= colNumber
+                        ) {
                     continue;
                 }
-                // Check that the new cell hasn't already been visited
-                if (reachable[newRow][newCol]) {
+                if(visited[newRow][newCol]){
                     continue;
                 }
-                // Check that the new cell has a higher or equal height,
-                // So that water can flow from the new cell to the old cell
-                if (landHeights[newRow][newCol] >= landHeights[cell[0]][cell[1]]) {
-                    queue.offer(new int[]{newRow, newCol});
+                if (matrix[newRow][newCol] >= matrix[cell[0]][cell[1]]) {
+                    queue.offer(new int[] { newRow, newCol });
                 }
-                // If we've gotten this far, that means the new cell is reachable
-               
             }
         }
-        return reachable;
+        return visited;
+
     }
 }
