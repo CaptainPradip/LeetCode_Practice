af76f9a881e17f8e466704c1179d100db55b7eca
Pradip Nemane <pradipnemane@gmail.com>
2024-08-29 00:39:36 -0400

    Time: 31 ms (15.97%), Space: 44.3 MB (77.62%) - LeetHub

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class FamilyNode {
+    public int val;
+    public Node parent;
+		Public List<Node> childerns;
+		FamilyNode(String name){
+			this.parent= null;
+		}
+		addchild
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+       HashSet<Node> hash = new HashSet<Node>();
+       while(p!=null){
+           hash.add(p);
+           p = p.parent;
+       }
+
+       while(q!=null){
+           if(hash.contains(q)){
+               return q;
+           }
+           q = q.parent;
+       }
+        
+        return q;
+    }
+		
+}
