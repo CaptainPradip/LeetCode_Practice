5c3bfb8ac13678f60b8476f3a60b455eefac52af
Pradip Nemane <pradipnemane@gmail.com>
2024-08-31 16:35:54 -0400

    Time: 0 ms (100%), Space: 42.3 MB (17.93%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,43 +1,42 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public void flatten(TreeNode root) {
         DFS(root);
     }
-    public TreeNode DFS(TreeNode root){
-        if(root==null){
+
+    public TreeNode DFS(TreeNode root) {
+        if (root == null) {
             return root;
         }
-
         TreeNode leftNode = DFS(root.left);
         TreeNode rightNode = DFS(root.right);
-
-        if(leftNode!=null){
+        if (leftNode != null) {
             leftNode.right = root.right;
-            root.right= root.left;
-            root.left= null;
+            root.right = root.left;
+            root.left = null;
         }
-        if(rightNode!=null){
+        if (rightNode != null) {
             return rightNode;
         }
-        if(leftNode!=null){
+        if (leftNode != null) {
             return leftNode;
         }
-        
-    return root;
 
+        return root;
     }
+
 }
