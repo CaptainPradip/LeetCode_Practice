99058be0f764d6a553a03d1a9eb2661f633fb93c
Pradip Nemane <pradipnemane@gmail.com>
2024-08-31 16:21:13 -0400

    Time: 2 ms (69.3%), Space: 43.8 MB (96.75%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -21,23 +21,21 @@ class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         // 1. Create a map with the value with index
         // 2. create a recursion funtion for the build the tree;
-        //
         n = inorder.length;
         for (int i = 0; i < n; i++) {
             inorderMap.put(inorder[i], i);
         }
-      return  buildTree(preorder, inorder, 0, n - 1);
+        return buildTree(preorder, inorder, 0, n - 1);
     }
 
-    public TreeNode buildTree(int[] preorder, int[] inorder, int start, int end ){
-
-        if(start > end){
+    public TreeNode buildTree(int[] preorder, int[] inorder, int start, int end) {
+        if (start > end) {
             return null;
         }
         TreeNode root = new TreeNode(preorder[rootIndex++]);
         int nextIndex = inorderMap.get(root.val);
-        root.left = buildTree(preorder,inorder,start,nextIndex-1);
-        root.right = buildTree(preorder,inorder,nextIndex+1, end  );
+        root.left = buildTree(preorder, inorder, start, nextIndex - 1);
+        root.right = buildTree(preorder, inorder, nextIndex + 1, end);
         return root;
     }
 }
