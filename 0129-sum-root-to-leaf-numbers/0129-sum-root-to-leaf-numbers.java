507161fa01f8bca7f06f70f8affd3d9deb6ee848
Pradip Nemane <pradipnemane@gmail.com>
2024-08-31 23:06:16 -0400

    Time: 0 ms (100%), Space: 41.1 MB (56.95%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -1,41 +1,44 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
-      return  sumNumbers(root, new LinkedList<>());
+
+        return backtracking(root, new ArrayList<>());
+
     }
-    // this is example of backtracking 
-    public int sumNumbers(TreeNode root,LinkedList<Integer> list){
-        if(root==null){
+
+    public int backtracking(TreeNode root, List<Integer> list) {
+
+        if (root == null) {
             return 0;
         }
         list.add(root.val);
-        if(root.left==null && root.right==null){
-            int sum =0 ;
-            for(int n : list){
-                sum=sum*10 + n;
+        if (root.left == null && root.right == null) {
+            int sum = 0;
+            for (int n : list) {
+                sum = sum * 10 + n;
             }
-             list.removeLast();
+            list.removeLast();
             return sum;
-           
         }
-       int leftSum=  sumNumbers(root.left,list);
-       int rightSum = sumNumbers(root.right,list);
-       list.removeLast();
-       return leftSum + rightSum;
+
+        int leftSum = backtracking(root.left, list);
+        int rightSum = backtracking(root.right, list);
+        list.removeLast();
+        return leftSum + rightSum;
 
     }
 }
