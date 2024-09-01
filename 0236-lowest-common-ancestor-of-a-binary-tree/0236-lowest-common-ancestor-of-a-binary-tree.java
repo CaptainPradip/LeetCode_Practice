521ba4050a2ee3be031188beaf15bde8cb5db297
Pradip Nemane <pradipnemane@gmail.com>
2024-09-01 02:00:44 -0400

    Time: 13 ms (7.88%), Space: 45.3 MB (12.63%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,10 +1,10 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
@@ -13,25 +13,25 @@ class Solution {
         Map<TreeNode, TreeNode> parents = new HashMap<>();
         Set<TreeNode> ancesstor = new HashSet<>();
         stack.push(root);
-        parents.put(root,null);
-        while(!parents.containsKey(p)|| !parents.containsKey(q)){
+        parents.put(root, null);
+        while (!parents.containsKey(p) || !parents.containsKey(q)) {
             TreeNode currentNode = stack.pop();
-            if(currentNode.left!=null){
-                parents.put(currentNode.left,currentNode);
+            if (currentNode.left != null) {
+                parents.put(currentNode.left, currentNode);
                 stack.push(currentNode.left);
             }
-             if(currentNode.right!=null){
-                parents.put(currentNode.right,currentNode);
+            if (currentNode.right != null) {
+                parents.put(currentNode.right, currentNode);
                 stack.push(currentNode.right);
             }
         }
-        while(p!=null){
-            ancesstor.add(p);
-            p = parents.get(p);
-        }
-         while(!ancesstor.contains(q)){
+        while (q != null) {
+            ancesstor.add(q);
             q = parents.get(q);
         }
-        return q;
+        while (!ancesstor.contains(p)) {
+            p = parents.get(p);
+        }
+        return p;
     }
 }
