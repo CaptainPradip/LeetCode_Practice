a75da479d31d30a4b2953e6825d3034b07938ddf
Pradip Nemane <pradipnemane@gmail.com>
2024-09-02 16:25:08 -0400

    Time: 15 ms (37.87%), Space: 48.8 MB (24.48%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,20 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // 1. Create HashMap with frequency
+        // 2. Create minheap size k as it will keep only k element
+        // 3. create the result array
+        // Time Complexity n logn
         HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int num : nums) {
-            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        for (int n : nums) {
+            hashMap.put(n, hashMap.getOrDefault(n, 0) + 1);
         }
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> hashMap.get(a) - hashMap.get(b));
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> hashMap.get(a) - hashMap.get(b));
         for (int key : hashMap.keySet()) {
-            minHeap.add(key);
-            if (minHeap.size() > k) {
-                minHeap.remove();
+            queue.add(key);
+            if (queue.size() > k) {
+                queue.remove();
             }
         }
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            result[i] = minHeap.remove();
+            result[i] = queue.remove();
         }
         return result;
+
     }
 }
