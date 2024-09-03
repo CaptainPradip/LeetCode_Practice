9c1be23e4330795cced4b3e45783dd20ba150867
Pradip Nemane <pradipnemane@gmail.com>
2024-09-02 21:55:39 -0400

    Time: 176 ms (5.09%), Space: 46.2 MB (5.56%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,15 +1,29 @@
 class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
+
     public int coinChange(int[] coins, int amount) {
-        int [] dp = new int [amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0] = 0;
-        for(int i=1; i<= amount;i++){
-            for(int coin: coins){
-                if(i-coin >= 0){
-                    dp[i]= Math.min(dp[i],dp[i-coin] + 1);
+        if (memo.containsKey(amount)) {
+            return memo.get(amount);
+        }
+        if (amount == 0) {
+            return 0;
+        }
+        if (amount < 0) {
+            return -1;
+        }
+        int minSize = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int remainningAmount = amount - coin;
+            int size = coinChange(coins, remainningAmount);
+            if (size != -1) {
+                if (minSize > size) {
+                    minSize = size + 1;
                 }
+
             }
         }
-        return dp[amount]== (amount+1)?-1:dp[amount];
+        minSize = minSize == Integer.MAX_VALUE ? -1 : minSize;
+        memo.put(amount, minSize);
+        return memo.get(amount);
     }
 }
