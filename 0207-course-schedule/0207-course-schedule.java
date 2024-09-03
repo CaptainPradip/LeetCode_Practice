1bccc271086df2a4762c1de20b3b0ade206a860e
Pradip Nemane <pradipnemane@gmail.com>
2024-09-03 10:43:11 -0400

    Time: 5 ms (67.17%), Space: 44.5 MB (69.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,39 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // 1. create a Graph
+        // 2. check indegree
+        // 3. add all 0 degree nodes to queue
+        // 4. once visited then reduce the degree
+        // 5. check the all visited nodes with total nodes
         List<List<Integer>> graph = new ArrayList<>();
-		for(int i=0;i< numCourses;i++){
-			graph.add(new ArrayList<>());
-		}
-		int[] indegree = new int [numCourses];
-		for(int[] prerequisite: prerequisites){
-			int start = prerequisite[1];// its start node 
-			int end = prerequisite[0] ;// as the course end
-			graph.get(start).add(end);
-			indegree[end]++;
-		}
-		// add all 0 indegree nodes in queue;
-		Queue<Integer> queue = new LinkedList<>();
-		int visitedNode = 0; 
-		for(int i=0;i< numCourses;i++){
-			if(indegree[i]==0){
-				queue.add(i);
-			}
-		}
-		
-		while(!queue.isEmpty()){
-			int currentNode = queue.remove();
-			visitedNode++;
-			for(int neibor : graph.get(currentNode)){
-				indegree[neibor]--;
-				if(indegree[neibor]==0){
-					queue.add(neibor);
-				}
-			}
-
-		}
-		return visitedNode == numCourses;
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        int[] indegree = new int[numCourses];
+        for (int[] pre : prerequisites) {
+            int start = pre[0];
+            int end = pre[1];
+            graph.get(start).add(end);
+            indegree[end]++;
+        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        int visitedNodes = 0;
+        while (!queue.isEmpty()) {
+            int currentNode = queue.remove();
+            visitedNodes++;
+            for (int nei : graph.get(currentNode)) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    queue.add(nei);
+                }
+            }
+        }
+        return visitedNodes == numCourses;
     }
+
 }
