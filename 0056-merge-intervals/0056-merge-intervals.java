c523030bdc944262f304dffb43e6a5596bfd2fe6
Pradip Nemane <pradipnemane@gmail.com>
2024-09-06 17:23:22 -0400

    Time: 8 ms (85.83%), Space: 46.6 MB (29.04%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        LinkedList<int[]> result = new LinkedList<>();
-        // 1. Sort the array with the start time
+        // 1. Sort the Array O(nlog(n))
+        // 2. Merge the array if the interval is in-between O(n)
+        // Time Complexity O(nlog(n))
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        // check for end with the start with second
-        // if( first end is more than the second start that mean it will merge)
-        // else add the second interval
+        List<int[]> result = new ArrayList<>();
         for (int[] interval : intervals) {
-
-            if (result.isEmpty() || interval[0] > result.getLast()[1]) {
+            if (result.isEmpty() || result.getLast()[1] < interval[0]) {
                 result.add(interval);
             } else {
-                result.getLast()[1] =  Math.max(result.getLast()[1], interval[1]);
+                result.getLast()[1] = Math.max(result.getLast()[1], interval[1]);
             }
         }
+
         return result.toArray(new int[result.size()][]);
+
     }
 }
