9706743178b0fed1ada8c17fbdf08cb04a4bb297
Pradip Nemane <pradipnemane@gmail.com>
2024-09-11 19:08:33 -0400

    Time: 48 ms (51.31%), Space: 114.7 MB (46.81%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,28 +1,29 @@
 class LRUCache {
     int capacity;
-    LinkedHashMap<Integer,Integer> cache;
+    LinkedHashMap<Integer, Integer> cache;
+
     public LRUCache(int capacity) {
         this.cache = new LinkedHashMap<>(capacity);
         this.capacity = capacity;
     }
-    
+
     public int get(int key) {
-        if(cache.containsKey(key)){
+        if (cache.containsKey(key)) {
             int value = cache.get(key);
             cache.remove(key);
-            cache.put(key,value);
+            cache.put(key, value);
             return value;
         }
         return -1;
     }
-    
+
     public void put(int key, int value) {
-         if(cache.containsKey(key)){
+        if (cache.containsKey(key)) {
             cache.remove(key);
-         } else if(cache.size()==this.capacity) {
+        } else if (cache.size() == this.capacity) {
             cache.remove(cache.keySet().iterator().next());
-         }
-        cache.put(key,value);
+        }
+        cache.put(key, value);
     }
 }
 
