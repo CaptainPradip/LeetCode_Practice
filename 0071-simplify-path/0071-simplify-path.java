a02b686546a1776a136e27493d539eea6474bd56
Pradip Nemane <pradipnemane@gmail.com>
2024-09-11 19:07:03 -0400

    Time: 4 ms (86.56%), Space: 42.9 MB (96.86%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,23 +1,26 @@
 class Solution {
     public String simplifyPath(String path) {
+        // 1. split with "/"
+        // 2. check for . || empty
+        // 3. check for .. then pop if stack having not empty value
+        // 4. other push on stack
         Stack<String> stack = new Stack<>();
-		String[] component = path.split("/");
-		for(String direc : component){
-			if(direc.equals(".") || direc.isEmpty()){
-				continue;
-			} else if(direc.equals("..")){
-				if(!stack.isEmpty()){
-					stack.pop();
-				}
-			} else{
-				stack.push(direc);
-			}
-		}
-		StringBuilder sb = new  StringBuilder();
-		for(String dir: stack){
-				sb.append("/");
-				sb.append(dir);
-		}
-		return sb.isEmpty() ? "/": sb.toString();
+        String[] dirctories = path.split("/");
+        for (String dir : dirctories) {
+            if (dir.equals(".") || dir.isEmpty()) {
+                continue;
+            } else if (dir.equals("..")) {
+                if (!stack.isEmpty())
+                    stack.pop();
+            } else {
+                stack.push(dir);
+            }
+        }
+        StringBuilder sb = new StringBuilder("");
+        for (String str : stack) {
+            sb.append("/");
+            sb.append(str);
+        }
+        return sb.isEmpty() ? "/" : sb.toString();
     }
 }
