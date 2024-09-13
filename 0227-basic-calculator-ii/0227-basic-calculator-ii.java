5c2dad310b8b511b48e751475056c1af8cc7283a
Pradip Nemane <pradipnemane@gmail.com>
2024-09-13 18:53:14 -0400

    Time: 31 ms (19.07%), Space: 47.1 MB (15.26%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int currentNumber = 0;
+        char operation = '+';
+        int len = s.length();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                currentNumber = currentNumber * 10 + Integer.parseInt(ch + "");
+            }
+            if (!Character.isDigit(ch) && ch != ' ' || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                } else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operation = ch;
+                currentNumber = 0;
+            }
+
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+}
