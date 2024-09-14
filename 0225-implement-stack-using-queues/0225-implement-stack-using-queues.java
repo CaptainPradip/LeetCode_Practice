7fb740b1bd78c88aff551161b02778a7107a4808
Pradip Nemane <pradipnemane@gmail.com>
2024-09-14 13:22:47 -0400

    Time: 0 ms (100%), Space: 41.2 MB (67.27%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,46 @@
+class MyStack {
+    private Queue<Integer> q1 = new LinkedList<>();
+    private Queue<Integer> q2 = new LinkedList<>();
+    private int top;
+
+    public MyStack() {
+
+    }
+
+    public void push(int x) {
+        q2.add(x);
+        top = x;
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+    }
+
+    public int pop() {
+        int n = q1.remove();
+        if (!q1.isEmpty()) {
+            top = q1.peek();
+        }
+        return n;
+    }
+
+    public int top() {
+        return top;
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
