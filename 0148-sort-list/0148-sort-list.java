848411bef27fe774fe57c850e7063e48b3d7e6da
Pradip Nemane <pradipnemane@gmail.com>
2024-09-16 21:00:05 -0400

    Time: 12 ms (32.27%), Space: 57.5 MB (9.56%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,53 +1,67 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-			if(head==null || head.next == null){
-				return head;
-			}
-      ListNode mid =  findMid(head);
-	  ListNode left= sortList(head);
-	  ListNode right = sortList(mid);
-	  return mergeList(left, right);
+        // 1. use sorting algoritham
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = findMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+    // [-1,5,3,4,0]
+
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prevNode = null;
+        while (fast != null && fast.next != null) {
+            prevNode = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(prevNode!=null)
+            prevNode.next = null;
+        return slow;
     }
 
-	public ListNode mergeList(ListNode list1, ListNode list2){
+    public ListNode merge(ListNode left, ListNode right) {
+        ListNode trav1 = left;
+        ListNode trav2 = right;
+        ListNode head = new ListNode(0);
+        ListNode trav = head;
+        while (trav1 != null && trav2 != null) {
+            if (trav1.val < trav2.val) {
+                trav.next = trav1;
+                trav1 = trav1.next;
+            } else {
+                trav.next = trav2;
+                trav2 = trav2.next;
+            }
+            trav = trav.next;
+        }
+        while (trav1 != null) {
+            trav.next = new ListNode(trav1.val);
+             trav =  trav.next;
+            trav1 = trav1.next;
+        }
+        while (trav2 != null) {
+            trav.next = new ListNode(trav2.val);
+             trav =  trav.next;
+            trav2 = trav2.next;
+        }
+        return head.next;
 
-		ListNode dummy = new ListNode();
-		ListNode tail = dummy;
-		while(list1!=null && list2!=null){
-				if(list1.val< list2.val){
-					tail.next = list1 ;
-					list1 = list1.next;
-				} else{
-					tail.next = list2;
-					list2 = list2.next;
-				}
-				tail= tail.next;		
-		}
-	    tail.next = list1!=null ? list1: list2;
-		return  dummy.next;
-		
-	}
-	public ListNode findMid(ListNode head){
-		ListNode slow = head;
-		ListNode fast = head;
-		ListNode preMid = null;
-		while(fast !=null && fast.next !=null){
-			fast = fast.next.next;
-			preMid = slow;
-			slow = slow.next;
-		}
-		if(preMid!=null)
-			preMid.next=null;
-		return slow ;
-	}
+    }
 }
