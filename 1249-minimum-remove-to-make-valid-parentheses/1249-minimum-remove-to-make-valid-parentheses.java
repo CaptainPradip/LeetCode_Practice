0f1c791f7c804d6d8d1915a7fa73f10f98ad35f0
Pradip Nemane <pradipnemane@gmail.com>
2024-09-20 20:23:50 -0400

    Time: 608 ms (5.04%), Space: 46.6 MB (6.32%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,32 +1,33 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Set<Integer> removeIndex = new HashSet<>();
         Stack<Integer> stack = new Stack<>();
         int n = s.length();
-        int i = 0 ;
-        while(i<n){
+        int i = 0;
+        while (i < n) {
             char ch = s.charAt(i);
-            if(ch=='('){
-                stack.add(i);
-            } else if(ch==')'){
-                if(!stack.isEmpty() && s.charAt(stack.peek())=='('){
+            if (ch == '(') {
+                stack.push(i);
+            } else if (ch == ')') {
+                if (!stack.isEmpty() && s.charAt(stack.peek()) == '(') {
                     stack.pop();
                 } else {
-                    stack.add(i);
+                    stack.push(i);
                 }
             }
             i++;
         }
-        while(!stack.isEmpty()){
+        Set<Integer> removeIndex = new HashSet<>();
+        while (!stack.isEmpty()) {
             removeIndex.add(stack.pop());
         }
-        StringBuilder sb = new StringBuilder();
-        for(i=0; i<s.length();i++){
-            if(!removeIndex.contains(i)){
-                sb.append(s.charAt(i));
+        String result = "";
+        for (int j = 0; j < n; j++) {
+            if (!removeIndex.contains(j)) {
+                result += s.charAt(j);
             }
         }
-        return sb.toString();
+
+        return result;
 
     }
 }
