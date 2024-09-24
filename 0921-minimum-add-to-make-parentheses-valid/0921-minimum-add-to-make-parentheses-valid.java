86ad7ce09f2cd88ca6b350b7851bd3bc44ef9a40
Pradip Nemane <pradipnemane@gmail.com>
2024-09-24 14:57:53 -0400

    Time: 2 ms (28.99%), Space: 41.5 MB (28.98%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,14 +1,29 @@
 class Solution {
+    // public int minAddToMakeValid(String s) {
+    // int count = 0;
+    // int close = 0;
+    // for (int i = 0; i < s.length(); i++) {
+    // close += s.charAt(i) == '(' ? 1 : -1;
+    // if (close == -1) {
+    // count++;
+    // close++;
+    // }
+    // }
+    // return count + close;
+    // }
     public int minAddToMakeValid(String s) {
-        int count = 0;
-        int close = 0;
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            close += s.charAt(i) == '(' ? 1 : -1;
-            if (close == -1) {
-                count++;
-                close++;
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                    continue;
+                }
+                stack.push(s.charAt(i));
             }
         }
-        return count + close;
+        return stack.size();
     }
 }
