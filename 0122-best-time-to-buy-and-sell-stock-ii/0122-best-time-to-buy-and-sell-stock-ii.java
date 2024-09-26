eaf78b2a0d698ef99dac14da04bb11b23484c803
Pradip Nemane <pradipnemane@gmail.com>
2024-09-25 21:51:10 -0400

    Time: 1 ms (90.6%), Space: 45.6 MB (73.53%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int totalProfit=0 ;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i] > prices[i-1]){
-                totalProfit+= prices[i] - prices[i-1];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
             }
         }
-        return totalProfit;
+        return maxProfit;
     }
 }
