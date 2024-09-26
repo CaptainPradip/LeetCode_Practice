08f7d2c0a71190bfd9fd54da20b2d22d0f598025
Pradip Nemane <pradipnemane@gmail.com>
2024-09-25 22:29:52 -0400

    Time: 2 ms (72.86%), Space: 40.8 MB (99.1%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,13 +1,13 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int n= citations.length,i;
-        for(i=1; i<=n;i++){
-            if(citations[n-i]<i){
+        int i;
+        int n = citations.length;
+        for (i = 1; i <= n; i++) {
+            if (citations[n - i] < i) {
                 break;
             }
-
-        }return i-1;
-
+        }
+        return i - 1;
     }
 }
