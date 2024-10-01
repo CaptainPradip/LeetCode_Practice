e4a5af0d84c5912a4f2576a8a18b9b115078fcea
Pradip Nemane <pradipnemane@gmail.com>
2024-10-01 08:49:42 -0400

    Time: 662 ms (8.63%), Space: 41.7 MB (60.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,18 @@
 class Solution {
     int count;
     public int findTargetSumWays(int[] nums, int target) {
-         DFS(nums,0, 0,target);
-         return count;
+        DFS(nums, 0, 0, target);
+        return count;
     }
-    public void DFS(int[] nums, int i , int sum , int target){
-        if(i==nums.length){
-            if(sum==target){
+
+    public void DFS(int[] nums, int i, int sum, int target) {
+        if (i == nums.length) {
+            if (sum == target) {
                 count++;
             }
-        }else {
-            DFS(nums,i+1, sum+nums[i],target);
-             DFS(nums,i+1, sum-nums[i],target);
+        } else {
+            DFS(nums, i + 1, sum + nums[i], target);
+            DFS(nums, i + 1, sum - nums[i], target);
         }
     }
 }
