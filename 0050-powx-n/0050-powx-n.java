353698988ef620d420e31f165f1f07a15a57ba59
Pradip Nemane <pradipnemane@gmail.com>
2024-10-01 09:38:09 -0400

    Time: 0 ms (100%), Space: 41.9 MB (68.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,23 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        int  N = n;
-        if(n<0){
-            x= 1/x;
-            N=-N;
+        int N = n;
+        if (n < 0) {
+            x = 1 / x;
+            N = -N;
         }
-        return getPower(x,N);
+        return getPower(x, N);
     }
 
-    private double getPower(double x,int n){
+    private double getPower(double x, int n) {
 
-        if(n==0){
-          return 1.00; 
+        if (n == 0) {
+            return 1.00;
         }
-        double half = getPower(x,n/2);
-        if(n%2==0){
+        double half = getPower(x, n / 2);
+        if (n % 2 == 0) {
             return half * half;
-        } else{
-          return half * half * x;
+        } else {
+            return half * half * x;
         }
     }
 }
