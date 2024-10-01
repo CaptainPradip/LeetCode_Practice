f8e587320dcc5101b564bc4c4c3b34e2eeed10d5
Pradip Nemane <pradipnemane@gmail.com>
2024-10-01 09:09:33 -0400

    Time: 4 ms (99.36%), Space: 45.1 MB (91.05%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,7 +1,7 @@
 class Solution {
-    private boolean checkPalindrome(String s, int i , int j){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+    private boolean checkPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
             i++;
@@ -9,17 +9,18 @@ class Solution {
         }
         return true;
     }
+
     public boolean validPalindrome(String s) {
-        int i =0;
-        int j = s.length()-1;
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
-               return checkPalindrome(s,i,j-1) || checkPalindrome(s,i+1,j);
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return checkPalindrome(s, i, j - 1) || checkPalindrome(s, i + 1, j);
             }
             i++;
             j--;
         }
         return true;
-        
+
     }
 }
