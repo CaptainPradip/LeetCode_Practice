8d5a1309b333d25009722edc795d9d12ea3824cf
Pradip Nemane <pradipnemane@gmail.com>
2024-10-01 09:15:27 -0400

    Time: 31 ms (55.76%), Space: 53.4 MB (44.28%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -3,12 +3,12 @@ class Solution {
 
         List<int[]> result = new ArrayList<>();
         PriorityQueue<int[]> minHeap = new PriorityQueue<>(
-                (a, b) ->  (b[0] * b[0] + b[1] * b[1])- (a[0] * a[0] + a[1] * a[1]));
+                (a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
 
         for (int i = 0; i < points.length; i++) {
             minHeap.add(points[i]);
-            if(minHeap.size()>k){
-               minHeap.remove(); 
+            if (minHeap.size() > k) {
+                minHeap.remove();
             }
         }
 
