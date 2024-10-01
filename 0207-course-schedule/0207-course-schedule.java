b7ea6c61b919a737c08eb2c16a0c6c5c53949ef5
Pradip Nemane <pradipnemane@gmail.com>
2024-10-01 12:51:37 -0400

    Time: 4 ms (82.53%), Space: 45.1 MB (53.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,42 @@
+
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // 1. create a Graph
-        // 2. check indegree
-        // 3. add all 0 degree nodes to queue
-        // 4. once visited then reduce the degree
-        // 5. check the all visited nodes with total nodes
+
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
             graph.add(new ArrayList<>());
         }
-        int[] indegree = new int[numCourses];
         for (int[] pre : prerequisites) {
-            int start = pre[0];
-            int end = pre[1];
+            int start = pre[1];
+            int end = pre[0];
             graph.get(start).add(end);
-            indegree[end]++;
         }
-        Queue<Integer> queue = new ArrayDeque<>();
-        for (int i = 0; i < indegree.length; i++) {
-            if (indegree[i] == 0) {
-                queue.add(i);
+        boolean[] visited = new boolean[numCourses];
+        boolean[] instack = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (isCycle(i, graph, visited, instack)) {
+                return false;
             }
         }
-        int visitedNodes = 0;
-        while (!queue.isEmpty()) {
-            int currentNode = queue.remove();
-            visitedNodes++;
-            for (int nei : graph.get(currentNode)) {
-                indegree[nei]--;
-                if (indegree[nei] == 0) {
-                    queue.add(nei);
-                }
-            }
-        }
-        return visitedNodes == numCourses;
+        return true;
     }
 
+    public boolean isCycle(int node, List<List<Integer>> adj, boolean[] visited, boolean[] instack) {
+        if (instack[node]) {
+            return true;
+        }
+        if (visited[node]) {
+            return false;
+        }
+        instack[node] = true;
+        visited[node] = true;
+        for (int nei : adj.get(node)) {
+            if (isCycle(nei, adj, visited, instack)) {
+                return true;
+            }
+        }
+        instack[node] = false;
+        return false;
+
+    }
 }
