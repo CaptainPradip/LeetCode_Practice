b22e7fed2615aeced4df1f6d07dacb16c3dff00e
Pradip Nemane <pradipnemane@gmail.com>
2024-10-01 10:19:00 -0400

    Time: 1 ms (46.75%), Space: 51.5 MB (9.39%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -1,33 +1,32 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        return sumBST(root,low ,high, 0);
+        return sumBST(root, low, high, 0);
     }
 
-    public int sumBST(TreeNode root,int low , int high,int sum){
-        if(root==null){
+    public int sumBST(TreeNode root, int low, int high, int sum) {
+        if (root == null) {
             return sum;
         }
-        if(root.val >=low && root.val<= high){
-            sum+=root.val;
+        if (root.val >= low && root.val <= high) {
+            sum += root.val;
         }
-          sum = sumBST(root.right,low,high, sumBST(root.left,low,high,sum));
+        sum = sumBST(root.right, low, high, sumBST(root.left, low, high, sum));
         return sum;
-        
 
     }
 }
