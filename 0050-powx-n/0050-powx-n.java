f894e34fb790189a981577530409efc26e9eb969
Pradip Nemane <pradipnemane@gmail.com>
2024-10-02 08:29:30 -0400

    Time: 0 ms (100%), Space: 42 MB (61.64%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,11 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        int N = n;
-        if (n < 0) {
+      long N = n;  // Use long to avoid overflow when negating the minimum integer value
+        if (N < 0) {
             x = 1 / x;
             N = -N;
         }
-        return getPower(x, N);
+
+        double result = 1.0;
+        double currentProduct = x;
+
+        // Iterate while there are still bits to process in N
+        while (N > 0) {
+            if (N % 2 == 1) {  // If N is odd, multiply the current result by currentProduct
+                result *= currentProduct;
+            }
+            currentProduct *= currentProduct;  // Square the current product
+            N /= 2;  // Divide N by 2 (right shift in binary)
+        }
+
+        return result;
     }
 
     private double getPower(double x, int n) {
