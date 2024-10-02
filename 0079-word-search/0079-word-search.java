a4d825e2159aabe4fdba52d9f368b66d05610527
Pradip Nemane <pradipnemane@gmail.com>
2024-10-02 12:16:00 -0400

    Time: 138 ms (70.48%), Space: 41.8 MB (32.39%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,31 +1,33 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i=0; i< board.length;i++){
-            for(int j=0;j<board[i].length;j++){
-                if(board[i][j]== word.charAt(0) && help(board,word,i,j,0)){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0) && DFS(0, board, i, j, word)) {
                     return true;
                 }
             }
         }
         return false;
+
     }
-    public boolean help(char[][] board,String word,int i,int j, int index){
-        if(word.length()<=index){
+
+    public boolean DFS(int index, char[][] board, int i, int j, String word) {
+        if (index >= word.length()) {
             return true;
         }
-        if(i<0 || j<0 || i>=board.length || j>= board[i].length || board[i][j]=='#' || board[i][j]!= word.charAt(index)){
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '#'
+                || word.charAt(index) != board[i][j]) {
             return false;
         }
         char temp = board[i][j];
-        board[i][j]='#';
-        if(help(board,word, i+1,j,index+1)||
-        help(board,word, i-1,j,index+1)||
-        help(board,word, i,j+1,index+1)||
-        help(board,word, i,j-1,index+1)
-        ){
+        board[i][j] = '#';
+        if (DFS(index + 1, board, i + 1, j, word) || DFS(index + 1, board, i - 1, j, word) ||
+                DFS(index + 1, board, i, j + 1, word) || DFS(index + 1, board, i, j - 1, word)) {
             return true;
         }
-        board[i][j]=temp;
+        board[i][j] = temp;
         return false;
     }
+
 }
