2c9070c4c1537c70182da781a8e32d274532cc38
Pradip Nemane <pradipnemane@gmail.com>
2024-10-02 11:54:04 -0400

    Time: 195 ms (6.03%), Space: 45.9 MB (6.93%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,36 +1,35 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-      if(root==null){
-          return 0;
-      }
-        int dl=diameterOfBinaryTree(root.left);
-        int dr=diameterOfBinaryTree(root.right);
-        int cur= getLength(root.left) + getLength(root.right);
-        return Math.max(cur, Math.max(dl, dr));
-        
-    }
-
-    public int getLength(TreeNode root){
-        if(root==null){
+        if (root == null) {
             return 0;
         }
+        int dl = diameterOfBinaryTree(root.left);
+        int dr = diameterOfBinaryTree(root.right);
+        int cur = getLength(root.left) + getLength(root.right);
+        return Math.max(cur, Math.max(dl, dr));
 
-        return 1 + Math.max(getLength(root.left),getLength(root.right));
     }
- 
+
+    public int getLength(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(getLength(root.left), getLength(root.right));
+    }
+
 }
