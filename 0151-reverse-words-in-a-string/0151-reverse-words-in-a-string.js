d94b369a5526123762fc45b9a5e815b6eddaaa0d
Pradip Nemane <pradipnemane@gmail.com>
2025-01-01 19:58:36 -0500

    Time: 5 ms (21.84%), Space: 51.3 MB (26.63%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -2,6 +2,32 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-   return s.trim().split(" ").filter((str)=> str.trim().length).reverse().join(" ");
+var reverseWords = function (s) {
+    let arr = s.trim().split(" ");
+    let result = [];
+
+    let result1 = "";
+    arr.forEach(s => {
+        if (s.trim() != "") {
+            result.push(s);
+        }
+    })
+    let i = 0;
+    let j = result.length - 1;
+    while (i < j) {
+        let temp = result[i];
+        result[i] = result[j];
+        result[j] = temp;
+        i++;
+        j--;
+    }
+    result.forEach((s, index )=> {
+        result1 += s;
+        if(index!=result.length - 1){
+            result1 += " ";
+        }
+        
+    })
+    return result1;
+
 };
