e5bd34452e78388973aa7e9c2c41e52349c9bb81
Pradip Nemane <pradipnemane@gmail.com>
2025-02-16 22:18:28 -0500

    Time: 5 ms (72.74%), Space: 43.9 MB (87.8%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int sum = 0;
         long reverse = 0;
-        if(x<0) {
+        int init = x;
+
+        if (x < 0) {
             return false;
         }
-        int init = x;
-        while(x>0){
-            reverse=reverse*10+x%10;
-            x/=10;
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
         }
-        
-    return reverse==init;     
-  }
+        return init == reverse;
+    }
 }
