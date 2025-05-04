5b42ddb5cfcd35b2a795ccb85e946f7e0a39bbe9
Pradip Nemane <pradipnemane@gmail.com>
2025-05-04 11:50:36 -0400

    Time: 0 ms (100%), Space: 42 MB (8.06%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,35 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode trav = root;
         List<Integer> result = new ArrayList<>();
-       
-        return  DFS(root);
-    }
-    public List<Integer> DFS(TreeNode root){
-        if(root==null){
-            return new ArrayList<>();
+        while (trav != null || !stack.isEmpty()) {
+            if (trav != null) {
+                result.add(trav.val);
+                stack.push(trav);
+                trav = trav.left;
+            } else {
+                TreeNode temp = stack.pop();
+                trav = temp.right;
+            }
+
         }
-        List<Integer> leftList =  DFS(root.left);
-        List<Integer> rightList =  DFS(root.right);
-        List result = new ArrayList<>();
-        result.add(root.val);
-        result.addAll(leftList);
-        result.addAll(rightList);
-        return result; 
+        return result;
     }
-    
 }
