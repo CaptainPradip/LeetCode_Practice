de19183eed418691c9a79e2a8108026d01852c8c
Pradip Nemane <pradipnemane@gmail.com>
2025-05-04 11:54:46 -0400

    Time: 0 ms (100%), Space: 41.8 MB (44.7%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,16 +1,16 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
@@ -18,16 +18,16 @@ class Solution {
         Stack<TreeNode> stack = new Stack<>();
         TreeNode trav = root;
         List<Integer> result = new ArrayList<>();
-        while (trav != null || !stack.isEmpty()) {
-            if (trav != null) {
+        while(trav!=null || !stack.isEmpty()){
+
+            if(trav!=null){
                 result.add(trav.val);
                 stack.push(trav);
                 trav = trav.left;
-            } else {
-                TreeNode temp = stack.pop();
-                trav = temp.right;
+            }else{
+                
+                trav = stack.pop().right;
             }
-
         }
         return result;
     }
