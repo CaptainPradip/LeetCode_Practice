5b5823246e498ee52383ba9e59c7e02e8a97ce2e
Pradip Nemane <pradipnemane@gmail.com>
2025-05-20 22:53:35 -0400

    Time: 0 ms (100%), Space: 41.5 MB (87.66%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,34 +1,58 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head;
-        ListNode slow = head;
-        int i = 0;
-        while (fast != null && i < n) {
-            fast = fast.next;
-            i++;
+        if (head == null || (head.next == null && n == 1)) {
+            return null;
         }
 
-        if (fast == null) {
-            return head.next;
+        //Brute Force Method
+
+        //Reverse the list
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-        slow = head;
-        while (fast.next != null && fast!=null) {
-            fast = fast.next;
-            slow = slow.next;
+        //prev is new head now
+        ListNode newcurr = prev;
+        ListNode newprev = null;
+        int skip = 1;
+
+        if (n == 1) {
+            prev = prev.next; // Move head to the next node
+        } else {
+            while (skip < n) {
+                skip++;
+                newprev = newcurr;
+                newcurr = newcurr.next;
+            }
+            newprev.next = newcurr.next;
         }
 
-        slow.next = slow.next.next;
-        return head;
-        
+        ListNode newcurr2 = prev;
+        ListNode newprev2 = null;
+        ListNode newnext2 = null;
+        while (newcurr2 != null) {
+            newnext2 = newcurr2.next;
+            newcurr2.next = newprev2;
+            newprev2 = newcurr2;
+            newcurr2 = newnext2;
+        }
+        return newprev2;
+
     }
 }
