ad92c22ae6552ad42c9e0c34eea93dbba787c502
Pradip Nemane <pradipnemane@gmail.com>
2025-05-21 20:39:36 -0400

    Time: 4 ms (73.22%), Space: 44.8 MB (19.91%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,11 +11,9 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
-
         for (ListNode node : lists) {
-            if (node != null) {
+            if (node != null)
                 heap.add(node);
-            }
         }
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
@@ -29,5 +27,4 @@ class Solution {
         }
         return dummy.next;
     }
-
 }
