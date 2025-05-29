c27574e97b68834aad9b7738a208f8ab1124ff12
Pradip Nemane <pradipnemane@gmail.com>
2025-05-28 21:38:46 -0400

    Time: 8 ms (86.81%), Space: 46.2 MB (96.52%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,10 +1,12 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // 1. Sort the Array O(nlog(n))
-        // 2. Merge the array if the interval is in-between O(n)
-        // Time Complexity O(nlog(n))
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        if(intervals.length <= 1){
+            return intervals;
+        }
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> result = new ArrayList<>();
+        
         for (int[] interval : intervals) {
             if (result.isEmpty() || result.getLast()[1] < interval[0]) {
                 result.add(interval);
@@ -14,6 +16,5 @@ class Solution {
         }
 
         return result.toArray(new int[result.size()][]);
-
     }
 }
