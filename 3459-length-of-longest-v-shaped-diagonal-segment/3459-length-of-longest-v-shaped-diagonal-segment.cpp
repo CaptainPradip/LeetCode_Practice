e39e6d2afc8bda1aa754b37d6861268e4bc8c97b
Pradip Nemane <pradipnemane@gmail.com>
2025-05-29 23:00:54 -0400

    Time: 2245 ms (9.09%), Space: 94.6 MB (98.05%) - LeetHub

--- 3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,52 +1,45 @@
 class Solution {
 public:
-    int n;
-    int m;
-    int dx[4] = {-1,-1,1,1};
-    int dy[4] = {-1,1,1,-1};
-    int getAns(vector<vector<int>> &grid, int i, int j, int ch, int a){
-        int ans = 1;
-        if(ch == 1){
-            int nx = i + dx[(a + 1)%4];
-            int ny = j + dy[(a + 1)%4];
-            if(nx >= 0 && nx < n && ny >= 0 && ny < m){
-                if(grid[i][j] == 0 && grid[nx][ny] == 2){
-                    ans = max(ans, 1 + getAns(grid, nx, ny, 0, (a+1)%4));
-                }else if(grid[i][j] == 2 && grid[nx][ny] == 0){
-                    ans = max(ans, 1 + getAns(grid, nx, ny, 0, (a+1)%4));
-                }
-            }
+    int ans;
+    int solve(vector<vector<int>>&g,int i,int j,int p,int cur,int x){
+        if(i<0||j<0||i>=g.size()||j>=g[0].size()||(cur==0&&g[i][j]!=2)||(cur==2&&g[i][j]!=0))return 0;
+        int op1=0;
+        int op2=0;
+        if(p==0){
+            if(x==0)
+            op1+=1+solve(g,i-1,j-1,1,g[i][j],1);
+            op2=1+solve(g,i+1,j-1,p,g[i][j],x);
+        }else if(p==1){
+            if(x==0)
+            op1+=1+solve(g,i-1,j+1,2,g[i][j],1);
+            op2=1+solve(g,i-1,j-1,p,g[i][j],x);
+        }else if(p==2){
+            if(x==0)
+            op1+=1+solve(g,i+1,j+1,3,g[i][j],1);
+            op2=1+solve(g,i-1,j+1,p,g[i][j],x);
+        }else if(p==3){
+            if(x==0)
+            op1+=1+solve(g,i+1,j-1,0,g[i][j],1);
+            op2=1+solve(g,i+1,j+1,p,g[i][j],x);
         }
-        int nx = i + dx[a];
-        int ny = j + dy[a];
-        if(nx >= 0 && nx < n && ny >= 0 && ny < m){
-            if(grid[i][j] == 0 && grid[nx][ny] == 2){
-                ans = max(ans, 1 + getAns(grid, nx, ny, ch, a));
-            }else if(grid[i][j] == 2 && grid[nx][ny] == 0){
-                ans = max(ans, 1 + getAns(grid, nx, ny, ch, a));
-            }
-        }
-        return ans;
+       
+        return max(op1,op2);
     }
-
     int lenOfVDiagonal(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1){
-                    ans = max(ans, 1);
-                    for(int a = 0; a < 4; a++){
-                        int nx = i + dx[a];
-                        int ny = j + dy[a];
-                        if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 2){
-                             ans = max(ans, 1 + getAns(grid, nx, ny, 1, a));
-                        }
-                    }
+        ans=-1;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    int a=0,b=0,c=0,d=0;
+                    a=solve(grid,i+1,j-1,0,0,0);
+                    b=solve(grid,i-1,j-1,1,0,0);
+                    c=solve(grid,i-1,j+1,2,0,0);
+                    d=solve(grid,i+1,j+1,3,0,0);
+                    ans=max({ans,a,b,c,d});
+
                 }
             }
         }
-        return ans;
+        return ans+1;
     }
 };
