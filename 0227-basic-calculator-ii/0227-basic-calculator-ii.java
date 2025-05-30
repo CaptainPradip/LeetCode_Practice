5317a0078ded64f7e333d14f0c8840cf6d36099d
Pradip Nemane <pradipnemane@gmail.com>
2025-05-29 22:12:17 -0400

    Time: 17 ms (70.25%), Space: 44.8 MB (93.79%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,36 +1,46 @@
 class Solution {
     public int calculate(String s) {
-        if (s == null || s.isEmpty()) {
-            return 0;
-        }
-        Stack<Integer> stack = new Stack<>();
-        int currentNumber = 0;
-        char operation = '+';
-        int len = s.length();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
-                currentNumber = currentNumber * 10 + Integer.parseInt(ch + "");
-            }
-            
-            if (!Character.isDigit(ch) && ch != ' ' || i == len - 1) {
-                if (operation == '-') {
-                    stack.push(-currentNumber);
-                } else if (operation == '+') {
-                    stack.push(currentNumber);
-                } else if (operation == '*') {
-                    stack.push(stack.pop() * currentNumber);
-                } else if (operation == '/') {
-                    stack.push(stack.pop() / currentNumber);
-                }
-                operation = ch;
-                currentNumber = 0;
-            }
-        }
+        Stack<Integer> st = new Stack<>();
         int result = 0;
-        while (!stack.isEmpty()) {
-            result += stack.pop();
+        int number = 0;
+        char sign = '+';
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    number = number * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                i--;
+            }
+            if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
+                if (sign == '+') {
+                    //st.push(number);
+                    result += number;
+                    prev = number;
+                } else if (sign == '-') {
+                   // st.push(-number);
+                   result-=number;
+                   prev = - number;
+                } else if (sign == '*') {
+                   // st.push(st.pop() * number);
+                   result = result - prev + (prev * number);
+                   prev = prev * number;
+
+                } else if (sign == '/') {
+                    //st.push(st.pop() / number);
+                    result = result - prev + (prev / number);
+                    prev = prev / number;
+                }
+                sign = c;
+                number = 0;
+            }
         }
+        // // int result = 0;
+        // for (int sa : st) {
+        //     result += sa;
+        // }
         return result;
     }
 }
