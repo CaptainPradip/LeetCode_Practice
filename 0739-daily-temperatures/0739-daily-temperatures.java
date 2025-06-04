9cf63c8cd76c53342a117ef149d2ca465bc41e35
Pradip Nemane <pradipnemane@gmail.com>
2025-06-03 20:26:06 -0400

    Time: 62 ms (66.95%), Space: 63.7 MB (5.09%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -4,16 +4,16 @@ class Solution {
         // 1. create a stack with the index
         // 2. remove pop when temp is greater than the current
         // 3. add the result value with the poped index with previousDay;
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> maxstack = new Stack<>();
         int n = temperatures.length;
         int[] result = new int[n];
         for (int i = 0; i < n; i++) {
-            int currentTemp = temperatures[i];
-            while (!stack.isEmpty() && currentTemp > temperatures[stack.peek()]) {
-                int prevDay = stack.pop();
-                result[prevDay] = i - prevDay;
+            int currentTemp = temperatures[i] ;
+            while(!maxstack.isEmpty() && temperatures[maxstack.peek()] < currentTemp ){
+                int preday = maxstack.pop() ;
+                result[preday] =  i - preday;
             }
-            stack.push(i);
+            maxstack.push(i);
         }
         return result;
 
