a37f088e1f803a9179d42a925acfe174577ac366
Pradip Nemane <pradipnemane@gmail.com>
2025-06-04 19:26:30 -0400

    Time: 28 ms (89.64%), Space: 54.9 MB (89%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,18 +1,17 @@
 class StockSpanner {
-    Stack<int [] > stack = new Stack<>();
+    Stack<int[]> stack = new Stack<>();
 
     public StockSpanner() {
-        
+
     }
-    
+
     public int next(int price) {
         int ans = 1;
-        Stack<int [] > stack2 = stack; 
-        while(!stack.isEmpty() && stack.peek()[0]<=price){
+        Stack<int[]> stack2 = stack;
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
             ans += stack.pop()[1];
         }
-        stack.push(new int []{price , ans});
-
+        stack.push(new int[] { price, ans });
         return ans;
     }
 }
