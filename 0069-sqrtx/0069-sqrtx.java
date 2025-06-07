c60bdbcf6930d4db68b0a9677767d870715c432d
Pradip Nemane <pradipnemane@gmail.com>
2025-06-06 20:30:05 -0400

    Time: 1 ms (87.42%), Space: 41.4 MB (6.93%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,23 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-
         long left = 0;
         long right = x;
 
-        while (left < right) {
+        while (left <= right) {
             long mid = left + (right - left) / 2;
-            if (x == mid * mid) {
-                return (int)mid;
-            } else if (mid * mid > x) {
+            long squre = mid * mid;
+
+            if (squre == x) {
+                return (int) mid;
+            }
+            if (squre > x) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-
         if (left * left > x) {
-            return (int)left - 1;
+            return (int) left - 1;
         }
-        return  (int)left;
+        return (int) left;
+
     }
 }
