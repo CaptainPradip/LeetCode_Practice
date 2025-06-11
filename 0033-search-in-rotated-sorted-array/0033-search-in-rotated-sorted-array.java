b355d89124ac912caaabf1040fad91cb0d983c83
Pradip Nemane <pradipnemane@gmail.com>
2025-06-10 20:08:14 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -2,10 +2,8 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-
         while (left <= right) {
-            int mid = (left + right) / 2;
-
+            int mid = (right + left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[left] <= nums[mid]) {
@@ -15,7 +13,7 @@ class Solution {
                     left = mid + 1;
                 }
             } else {
-                if (nums[mid] <= target && target <= nums[right]) {
+                if (nums[left] >= target && target >= nums[mid]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
