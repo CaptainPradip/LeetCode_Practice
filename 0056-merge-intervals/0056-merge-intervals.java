722a935d065d49192ece0a5c34da049df95f475b
Pradip Nemane <pradipnemane@gmail.com>
2025-06-24 20:40:32 -0400

    Time: 8 ms (86.46%), Space: 46.3 MB (96.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length <= 1){
+        if (intervals.length <= 1) {
             return intervals;
         }
 
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> result = new ArrayList<>();
-        
+
         for (int[] interval : intervals) {
             if (result.isEmpty() || result.getLast()[1] < interval[0]) {
                 result.add(interval);
@@ -14,7 +14,6 @@ class Solution {
                 result.getLast()[1] = Math.max(result.getLast()[1], interval[1]);
             }
         }
-
         return result.toArray(new int[result.size()][]);
     }
 }
