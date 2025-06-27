c5a202da71fc00472b41b8a45212f9f234b4e44d
Pradip Nemane <pradipnemane@gmail.com>
2025-06-26 21:49:15 -0400

    Time: 0 ms (100%), Space: 40.2 MB (81.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,10 +4,10 @@ class Solution {
             return 1;
         }
         int[] dp = new int[n + 1];
-        dp[1] = 1; // 0-->1;
-        dp[2] = 2;// 0--> 1--> 2 or 0--->2
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        dp[1] = 1;
+        dp[2] =2;
+        for(int i=3 ;i<=n ;i++){
+            dp[i] = dp[i-1] +dp[i-2];
         }
         return dp[n];
     }
