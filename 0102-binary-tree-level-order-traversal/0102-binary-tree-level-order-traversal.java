c9c072983ef46693a1d3210fd93cabd4eb322d8a
Pradip Nemane <pradipnemane@gmail.com>
2025-07-01 21:50:45 -0400

    Time: 1 ms (90.58%), Space: 45.2 MB (35.63%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,32 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         Queue<TreeNode> queue = new ArrayDeque<>();
+
         if (root == null) {
             return result;
         }
         queue.add(root);
+
         while (!queue.isEmpty()) {
             int size = queue.size();
-            List<Integer> temp = new ArrayList<>();
+            List<Integer> result1 = new ArrayList<>();
             for (int i = 0; i < size; i++) {
                 TreeNode currentNode = queue.remove();
-                temp.add(currentNode.val);
+                result1.add(currentNode.val);
                 if (currentNode.left != null) {
                     queue.add(currentNode.left);
                 }
@@ -34,9 +36,8 @@ class Solution {
                     queue.add(currentNode.right);
                 }
             }
-            result.add(temp);
+            result.add(result1);
         }
         return result;
-
     }
 }
