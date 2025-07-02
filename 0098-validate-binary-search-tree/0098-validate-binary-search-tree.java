d301498224a701de82ba81da609ab666bf094c00
Pradip Nemane <pradipnemane@gmail.com>
2025-07-01 21:42:26 -0400

    Time: 0 ms (100%), Space: 43.2 MB (78.26%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,48 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-     return isValidBST1(root,null,null); 
+    TreeNode prev = null;
+ public boolean isValidBST(TreeNode root) {
+        prev = null;
+        return inOrder(root);
     }
-    public boolean isValidBST(TreeNode root,Integer low, Integer high ){
-        if(root==null){
+    public boolean inOrder(TreeNode root) {
+        if (root == null) {
             return true;
         }
-        
-        if((low != null && root.val<=low) || (high!=null && root.val>=high)){
+        if (!inOrder(root.left)) {
             return false;
         }
-        
-        return isValidBST(root.right, root.val, high ) && isValidBST(root.left, low, root.val);
-    }
-     
-    public boolean isValidBST1(TreeNode root, Integer min, Integer max) {
-        if(root==null){
-            return true;
-        }
-        if((min!=null && root.val <= min)||(max!=null && root.val >=max)){
+        if (prev != null && prev.val >= root.val) {
             return false;
         }
-        
-        return isValidBST1(root.right,root.val,max) && isValidBST1(root.left,min,root.val) ;
+        prev = root;
+
+        return inOrder(root.right);
     }
 }
-
-
-    // public boolean isValidBST(TreeNode root){
-        
-    //     return isValidBST1(root,null,null);
-    // }
