47e9817f3a31fdbf0d6d49882da2ae2f0487ebb5
Pradip Nemane <pradipnemane@gmail.com>
2025-07-17 20:53:42 -0400

    Time: 0 ms (100%), Space: 54.3 MB (68.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,13 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function(nums, target) {
- const map = {};
+    const pairIdx = {};
+
     for (let i = 0; i < nums.length; i++) {
-      if (map[target - nums[i]] !== undefined) {
-        return [map[target - nums[i]], i];
-      }
-      map[nums[i]] = i;
-    }
-    return [];
-};
+        const num = nums[i];
+        if (target - num in pairIdx) {
+            return [i, pairIdx[target - num]];
+        }
+        pairIdx[num] = i;
+    }    
+}
