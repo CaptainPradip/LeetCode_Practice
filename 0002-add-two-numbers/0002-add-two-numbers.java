c8f6bd01815700da6b01a6e610b7ca10d0a959e9
Pradip Nemane <pradipnemane@gmail.com>
2026-01-17 11:43:29 -0500

    Time: 1 ms (99.78%), Space: 46.9 MB (11.36%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,46 +1,33 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode newList = new ListNode(0);
-        ListNode head = newList;
+        ListNode l3 = new ListNode(0);
+        ListNode head = l3;
         int carry = 0;
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val + carry;
-            ListNode temp = new ListNode(sum % 10);
+
+        while(l1 != null || l2 != null || carry != 0){
+            int sum = carry;
+            if(l1 != null){
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+
             carry = sum / 10;
-            newList.next = temp;
-            l1 = l1.next;
-            l2 = l2.next;
-            newList = newList.next;
-        }
-        while (l1 != null) {
-            int sum = l1.val + carry;
-            ListNode temp = new ListNode(sum % 10);
-            carry = sum / 10;
-            newList.next = temp;
-            l1 = l1.next;
-            newList = newList.next;
-        }
-        while (l2 != null) {
-            int sum = l2.val + carry;
-            ListNode temp = new ListNode(sum % 10);
-            carry = sum / 10;
-            newList.next = temp;
-            l2 = l2.next;
-            newList = newList.next;
-        }
-        if (carry > 0) {
-            ListNode temp = new ListNode(carry);
-            newList.next = temp;
+            l3.next = new ListNode(sum%10);
+            l3 = l3.next;
         }
         return head.next;
     }
